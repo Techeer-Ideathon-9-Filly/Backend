6c48a3e301cd6f0114a60c407331078c0b635ff2
rvbear <rvbear2000@gmail.com>
2024-06-01 02:48:12 +0900

    feat: user 모델 생성

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models):
+    id = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=128)
+    nickname = models.CharField(max_length=20)
